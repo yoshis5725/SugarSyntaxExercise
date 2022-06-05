5f84fbad672918576bd585f84512e761eac73f0d
Elliott Polite <elliottp@iMacs-iMac.local>
2022-06-05 15:29:33 -0600

    completed exercise

---
+++ SugarSyntaxExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SugarSyntaxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            string greaterThanResponse = $"{answer} is greater than or equal to nine";
+            string lessThanResponse = $"{answer} is less than nine";
+
+            var answerToQuestion = (answer < 9) ? lessThanResponse : greaterThanResponse;
+
+            Console.Write(answerToQuestion);
+        }
+    }
+}
+
