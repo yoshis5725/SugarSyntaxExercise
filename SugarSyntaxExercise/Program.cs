abe6f05fd7e42f8cb83c8f70233c8988ca3d314e
Elliott Polite <elliottp@iMacs-iMac.local>
2022-06-05 15:32:35 -0600

    changed the variables from string to var, this is now completed

--- SugarSyntaxExercise/Program.cs
+++ SugarSyntaxExercise/Program.cs
@@ -7,8 +7,8 @@ namespace SugarSyntaxExercise
         static void Main(string[] args)
         {
             int answer = 4;
-            string greaterThanResponse = $"{answer} is greater than or equal to nine";
-            string lessThanResponse = $"{answer} is less than nine";
+            var greaterThanResponse = $"{answer} is greater than or equal to nine";
+            var lessThanResponse = $"{answer} is less than nine";
 
             var answerToQuestion = (answer < 9) ? lessThanResponse : greaterThanResponse;
 
